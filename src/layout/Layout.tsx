5f29ddf85a8a04015b625844d146b1e44b7f40eb
JPTeasdale <jpteasdale@gmail.com>
2022-07-08 09:17:18 -0700

    generator now generates outlined shapes and can contain other shapes

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -25,8 +25,10 @@ function generateLayout(canvas: fabric.Canvas) {
         return new fabric.Circle({
           top: randomInt(height),
           left: randomInt(width),
-          radius: randomIntFromInterval(10, 100),
-          fill: "black",
+          radius: randomIntFromInterval(10, 500),
+          stroke: "black",
+          strokeWidth: 2,
+          fill: "white",
         });
       case "line":
         return new fabric.Line(
@@ -38,6 +40,7 @@ function generateLayout(canvas: fabric.Canvas) {
           ],
           {
             stroke: "black",
+            strokeWidth: 2,
           }
         );
       case "triangle":
@@ -46,39 +49,48 @@ function generateLayout(canvas: fabric.Canvas) {
           left: randomInt(width),
           angle: randomIntFromInterval(0, 360),
           width: randomIntFromInterval(10, 100),
-          fill: "black",
+          stroke: "black",
+          strokeWidth: 2,
+          fill: "white",
         });
       case "rect":
         return new fabric.Rect({
           top: randomInt(height),
           left: randomInt(width),
-          height: randomIntFromInterval(10, 100),
-          width: randomIntFromInterval(10, 100),
-          fill: "black",
+          height: randomIntFromInterval(10, 400),
+          width: randomIntFromInterval(10, 400),
+          stroke: "black",
+          strokeWidth: 2,
+          fill: "white",
         });
     }
   }
 
   function addShape(): void {
     const newShape = generateRandomShape();
-    if(!newShape.isType('line')) {
-        const intersects = canvas.getObjects().some((obj) => !obj.isType('line') && newShape.intersectsWithObject(obj));
-        if (intersects) {
-            return addShape()
-        }
+    if (!newShape.isType("line")) {
+      const intersects = canvas
+        .getObjects()
+        .some(
+          (obj) =>
+            !obj.isType("line") &&
+            newShape.intersectsWithObject(obj) &&
+            !(newShape.isContainedWithinObject(obj) || obj.isContainedWithinObject(newShape))
+        );
+      if (intersects) {
+        return addShape();
+      }
     }
-
-    console.log("Adding shape", newShape)
     canvas.add(newShape);
   }
 
-  range(0, 5 + randomInt(10)).forEach(addShape);
+  range(0, 7 + randomInt(7)).forEach(addShape);
 }
 
 export function Layout() {
   const canvasEl = React.useRef(null);
   React.useEffect(() => {
-    const options = {backgroundColor: '#fff'};
+    const options = { backgroundColor: "#fff" };
     const canvas = new fabric.Canvas(canvasEl.current, options);
     generateLayout(canvas);
     return () => {
